ef7e43cb84d517cf9972f5339fb2a9336bcbea6e
Pranjal <PRANJAL.22948@stu.upes.ac.in>
2025-09-30 18:56:39 +0530

    100 days of code

---
+++ Q73.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+    int matrix[rows][cols], rowSum[rows];
+    for (int i = 0; i < rows; i++) {
+        rowSum[i] = 0;
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            rowSum[i] += matrix[i][j];
+        }
+    }
+    for (int i = 0; i < rows; i++)
+        printf("%d ", rowSum[i]);
+    printf("\n");
+    return 0;
+}
